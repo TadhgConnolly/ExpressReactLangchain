82f776976ba0be763cbd97f5cfb0cee6968d9e96
Tadhg <tadhg.connolly@gmail.com>
2024-04-01 20:01:02 +1300

    frontend talking to backend

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ const express = require('express');
 const app = express();
 const port = 3001;
 
+app.use(express.json());
+
 app.get('/', (req, res) => res.send('Hello Backend World!'));
 
 app.listen(port, () => console.log(`Listening on port ${port}!`));
+
+app.post('/send-message', (req, res) => {
+    console.log(req.body.message); // Later, replace this with function calls
+    res.status(200).send('Message received');
+  });
